771998f3f0d8cc2d626669c71e7033f2484c8a6b
RuttikHiwase <89509037+RuttikHiwase@users.noreply.github.com>
2024-08-27 16:06:13 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,155 +1,25 @@
-﻿using Delegates1;
-
-namespace Delegates1
+﻿namespace ImplicitVariables
 {
-    //step1 : create a delegate class that matches the func signature
-    public delegate void Del1();
-    public delegate int DelAdd(int a, int b);
-
-    //Object
-    //Delegate
-    //MulticastDelegate
-    //Del1
     internal class Program
     {
-        static void Main1()
+        
+        static void Main(string[] args)
         {
-            
-            //step 2 : create a delegate reference and
-            //point it to an object of the delegate class.
-            //The delegate object will take the func name as a parameter
+            //can only be used for local variables, not for class level variables,
+            //not for func parameters or return values
+            //int i = 100;
+            var i = 100;  //implicit variable
 
-            Del1 objDel = new Del1(Display);
+            //var j;  //error
 
-            //step 3: call the func via the delegate reference
-            objDel();
-        }
-        static void Main2()
-        {
-            Del1 objDel = Display;
-            objDel();
-            objDel = Show;
-            objDel();
-        }
-        static void Main3()
-        {
-            Del1 objDel = Display;
-            objDel();
+            var s = 1243;
+            //s = ""; //error
+            s = 100;
 
-            Console.WriteLine();
-            objDel += Show;
-            objDel();
+            int a = 100;
+            var b = 100;
 
-            Console.WriteLine();
-            objDel += Display;
-            objDel();
-
-            Console.WriteLine();
-            objDel -= Display;
-            objDel();
-
-
-            Console.WriteLine();
-            objDel -= Display;
-            objDel();
-
-            //Console.WriteLine();
-            //objDel -= Show;
-            //objDel();
-
-
-        }
-
-        static void Main4()
-        {
-            DelAdd oDelAdd = Add;
-            oDelAdd += Subtract;
-            Console.WriteLine(oDelAdd(10,5));
-        }
-        static void Main5()
-        {
-            Del1 objDel =(Del1) Delegate.Combine(new Del1(Display), new Del1(Show), new Del1(Display));
-            objDel();
-
-            Console.WriteLine();
-            //objDel = (Del1)Delegate.Remove(objDel, new Del1(Display));
-            objDel = (Del1)Delegate.RemoveAll(objDel, new Del1(Display));
-            objDel();
-        }
-
-        static void Display()
-        {
-            Console.WriteLine("display");
-        }
-        static void Display(string s)
-        {
-            Console.WriteLine("display"+ s);
-        }
-        static void Show()
-        {
-            Console.WriteLine("Show");
-        }
-        static int Add(int a, int b)
-        {
-            return a + b;
-        }
-        static int Subtract(int a, int b)
-        {
-            return a - b;
-        }
-        static void Main()
-        {
-            Del1 objDel = Class1.Display;
-            objDel();
-
-            Class1 o = new Class1();
-            objDel = o.Show;
+            Console.WriteLine(i.GetType());
         }
     }
-
-    public class Class1
-    {
-        public static void Display()
-        {
-            Console.WriteLine("class 1 display");
-        }
-        public void Show()
-        {
-            Console.WriteLine("class1 Show");
-        }
-    }
-}
-
-
-namespace Delegates2
-{
-    //step1 : create a delegate class that matches the func signature
-    public delegate int DelAdd(int a, int b);
-
-
-    internal class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine(CallMathOperation(Add,10,6));
-            Console.WriteLine(CallMathOperation(Subtract, 10, 3));
-        }
-        //pass a func as a parameter.
-        //called func receives it as a delegate
-        static int CallMathOperation(DelAdd objMath, int a, int b) 
-        {
-            return objMath(a,b);
-        }
-        static int Add(int a, int b)
-        {
-            return a + b;
-        }
-        static int Subtract(int a, int b)
-        {
-            return a - b;
-        }
-       
-    }
-
-   
 }
